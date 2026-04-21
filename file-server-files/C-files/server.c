52258e5f4fb48c35c872328e42cafa850c654ac5
LHospitalzRule <67443052+LHospitalzRule@users.noreply.github.com>
2026-04-21 10:17:29 -0400

    DEBUG_Basic

--- file-server-files/C-files/server.c
+++ file-server-files/C-files/server.c
@@ -1,6 +1,7 @@
 /* This is the server code */
 #include "file-server.h"
 #include <sys/fcntl.h>
+#include <arpa/inet.h>
 
 #define QUEUE_SIZE 10
 
@@ -12,8 +13,16 @@
 int main(int argc, char *argv[])
 {	
   int net_socket, b, l, fd, sa, bytes, on = 1;
+  int debugFlag = 0; /* monitoring DEBUG mode*/
   char buf[BUF_SIZE];		/* buffer for outgoing file */
-  struct sockaddr_in channel;		/* holds IP address */
+  struct sockaddr_in channel;		/* holds IP address for*/
+  struct sockaddr_in clientAddress;  /* Holds clientIP address*/
+  
+
+  /* Check for DEBUG flag */
+  if(argc==2 && strcmp(argv[1], "DEBUG=1") == 0){
+    debugFlag = 1;
+  }
 
   /* Build address structure to bind to socket. */
   memset(&channel, 0, sizeof(channel));	/* zero channel */
@@ -34,10 +43,15 @@ int main(int argc, char *argv[])
 
   /* Socket is now set up and bound. Wait for connection and process it. */
   while (1) {
-        sa = accept(net_socket, 0, 0);		/* block for connection request */
+        socklen_t clientLength = sizeof(clientAddress); /* observe address length */
+        sa = accept(net_socket, (struct sockaddr *) &clientAddress, &clientLength);		/* Save a section for distinct connection request */
         if (sa < 0) fatal("accept failed");
 
+        char *client_IP = inet_ntoa(clientAddress.sin_addr);
+
         read(sa, buf, BUF_SIZE);		/* read file name from socket */
+
+        if (debugFlag) printf("Sending %s to %s\n", buf, client_IP); 
 
         /* Get and return the file. */
         fd = open(buf, O_RDONLY);	/* open the file to be sent back */
@@ -48,6 +62,9 @@ int main(int argc, char *argv[])
                 if (bytes <= 0) break;		/* check for end of file */
                 write(sa, buf, bytes);		/* write bytes to socket */
         }
+
+        if (debugFlag) printf("Finished sending %s to %s\n", buf, client_IP);
+        
         close(fd);			/* close file */
         close(sa);			/* close connection */
   }
