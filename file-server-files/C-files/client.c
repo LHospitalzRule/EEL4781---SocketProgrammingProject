72427e0e8680e43c1118921e031cfbe311eb4d0f
LHospitalzRule <67443052+LHospitalzRule@users.noreply.github.com>
2026-04-22 13:50:06 -0400

    clientSide file not found message

--- file-server-files/C-files/client.c
+++ file-server-files/C-files/client.c
@@ -85,6 +85,15 @@ int main(int argc, char **argv)
     while (1) {
         bytes = read(net_socket, buf, BUF_SIZE); /* read a chunk from socket  */
         if (bytes <= 0) break;                   /* End if no bytes were observed in the socket */
+
+
+        if(strncmp(buf, "ERROR:", 6) == 0){ // checks for the first few words in the error m
+            printf("%s", buf);  /* print the error message */
+            fclose(outfile);
+            close(net_socket);
+            exit(1);
+        }
+
         fwrite(buf, 1, bytes, outfile);          /* write exactly those bytes into a file */
     }
  
